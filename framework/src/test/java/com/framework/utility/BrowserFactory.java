f46e226faddd7eaa1adcab9efefd579153d92fd1
Abhishek <pabhi255@gmail.com>
2020-07-24 01:50:35 +0530

    new

--- framework/src/test/java/com/framework/utility/BrowserFactory.java
+++ framework/src/test/java/com/framework/utility/BrowserFactory.java
@@ -11,7 +11,7 @@ public class BrowserFactory {
 	public static WebDriver startApplication(WebDriver driver,String browserName,String appURL)
 	
 	{
-	if(browserName.equals("Firefox"))	
+	if(browserName.equals("Firefox"))	//
 		
 	{
 		
